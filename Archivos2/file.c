10ab5bd8e27ab5c01f94068194ea65ea9742cc29
def4ultcode <61471484+def4ultcode@users.noreply.github.com>
2023-01-19 14:57:53 -0500

    Actualizacion

--- Archivos2/file.c
+++ Archivos2/file.c
@@ -3,25 +3,53 @@
 int main(){
     FILE *inputFile, *outputFile;
     int number;
+    char answer;
 
     inputFile = fopen("vector.txt", "r");
-    outputFile = fopen("output.txt", "w");
+    outputFile = fopen("output.txt", "r");
+    if(outputFile != NULL){
+        printf("El archivo output.txt ya existe ¿Desea reemplazarlo? (s/n)");
+        scanf("%c", &answer);
+        if(answer == 's'){
+            outputFile = fopen("output.txt", "w");
+            if (inputFile == NULL || outputFile == NULL){
+                printf("Error opening file\n");
+                return -1;
+            }
 
-    if (inputFile == NULL || outputFile == NULL){
-        printf("Error opening file\n");
-        return -1;
-    }
+            while (fscanf(inputFile, "%d", &number) == 1){
+                if (number % 3 == 0){
+                    fprintf(outputFile, "%d\n", number);
+                }
+            }
 
-    while (fscanf(inputFile, "%d", &number) == 1){
-        if (number % 3 == 0){
-            fprintf(outputFile, "%d\n", number);
+            fclose(inputFile);
+            fclose(outputFile);
+
+            printf("Realizado, por favor revisa el archivo output.txt");
+            return 0;
+        }else{
+            printf("No se ejecutara el programa y no se reemplazara nada.");
+            return -1;
         }
+    }else{
+        outputFile = fopen("output.txt", "w");
+        if (inputFile == NULL || outputFile == NULL){
+            printf("Error opening file\n");
+            return -1;
+        }
+
+        while (fscanf(inputFile, "%d", &number) == 1){
+            if (number % 3 == 0){
+                fprintf(outputFile, "%d\n", number);
+            }
+        }
+
+        fclose(inputFile);
+        fclose(outputFile);
+
+        printf("Realizado, por favor revisa el archivo output.txt");
+        return 0;
     }
-
-    fclose(inputFile);
-    fclose(outputFile);
-
-    printf("Realizado, por favor revisa el archivo output.txt");
-    return 0;
 }
 
